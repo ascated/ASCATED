e7e64a57325e7700b6b3a06b8011e74c96f2ed71
ascated <94944551+ascated@users.noreply.github.com>
2021-11-23 21:41:52 -0600

    Updates

---
+++ precache-manifest.6154bc6406660ce4123e1f76dbb31440.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcaa61945d2b82134f560875fe6d53de",
+    "url": "/ascated/index.html"
+  },
+  {
+    "revision": "0030de14121fd5f16020",
+    "url": "/ascated/static/css/main.da902447.chunk.css"
+  },
+  {
+    "revision": "d1e8de39a6d1bd195deb",
+    "url": "/ascated/static/js/2.2d016ddc.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/ascated/static/js/2.2d016ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0030de14121fd5f16020",
+    "url": "/ascated/static/js/main.ec1b4d4e.chunk.js"
+  },
+  {
+    "revision": "e46455eef080ca332757",
+    "url": "/ascated/static/js/runtime-main.0b42c916.js"
+  },
+  {
+    "revision": "ae37d4cac010ed65206036849fe64070",
+    "url": "/ascated/static/media/Isbelasoft - AD.ae37d4ca.mp4"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/ascated/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "94d09cb2e5039274f191c1a838d29060",
+    "url": "/ascated/static/media/feature-tile-icon-01.94d09cb2.svg"
+  },
+  {
+    "revision": "4441f1f963212189b8ead0520fa2a5f4",
+    "url": "/ascated/static/media/feature-tile-icon-02.4441f1f9.svg"
+  },
+  {
+    "revision": "565aafdc737b00f3d337106f67e00caa",
+    "url": "/ascated/static/media/feature-tile-icon-03.565aafdc.svg"
+  },
+  {
+    "revision": "cb405a4ea716bbe629d812a0f1c9c578",
+    "url": "/ascated/static/media/feature-tile-icon-04.cb405a4e.svg"
+  },
+  {
+    "revision": "22b8d2357439f817b89551a5e286cb0d",
+    "url": "/ascated/static/media/feature-tile-icon-05.22b8d235.svg"
+  },
+  {
+    "revision": "e72db84c01e737aca129cd0dba195daa",
+    "url": "/ascated/static/media/feature-tile-icon-06.e72db84c.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/ascated/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/ascated/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/ascated/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "32c459c09ae0935a07919e6c61574386",
+    "url": "/ascated/static/media/illustration-section-01.32c459c0.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/ascated/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "d7e08e8d963305f553af9d23d1c118ac",
+    "url": "/ascated/static/media/logo.d7e08e8d.svg"
+  },
+  {
+    "revision": "df8bde3ef9fb15a74443424aaff34ca6",
+    "url": "/ascated/static/media/logoCompleto.df8bde3e.svg"
+  }
+]);
